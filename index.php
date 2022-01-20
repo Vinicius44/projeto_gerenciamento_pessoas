cdb9bfc9716da8fb966aa8c9c3ef36ce6a28c407
Vinicius da Silva <msn-dovinicius@hotmail.com>
2022-01-20 12:51:27 -0300

    Criando .htaccess, config e index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include("config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gerenciamento de Pessoas</title>
+</head>
+<body>
+	<h1>Gerenciamento de Pessoas</h1>
+	<ul>
+		<li><a href="Cadastrar_Pessoas">Cadastrar Pessoas</a></li>
+		<li><a href="Modificar_Pessoas">Modificar Pessoas</a></li>
+		<li><a href="Excluir_Pessoas">Excluir Pessoas</a></li>
+		<li><a href="Ver_Pessoas">Ver Pessoas</a></li>
+	</ul>
+
+
+
+
+</body>
+</html>
