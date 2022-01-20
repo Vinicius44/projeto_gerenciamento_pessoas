dc4bf40eacc7093af44714da96edc93e94dd7e6b
Vinicius da Silva <msn-dovinicius@hotmail.com>
2022-01-20 13:55:39 -0300

    Modificando o CSS e o INDEX

--- index.php
+++ index.php
@@ -3,9 +3,26 @@
 <html>
 <head>
 	<title>Gerenciamento de Pessoas</title>
+
+	<!--Fonts-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,300&display=swap" rel="stylesheet">
+	<!---->
+
 	<link rel="stylesheet" type="text/css" href="<?php echo INCLUDE_PATH?>/css/style.css">
 </head>
 <body>
+
+
+	<?php
+
+		$url = isset($_GET["url"]) ? $_GET["url"] : "home";
+
+
+	?>
+
+	<div class="container">
 	<h1>Gerenciamento de Pessoas</h1>
 	<ul>
 		<li><a href="Cadastrar_Pessoas">Cadastrar Pessoas</a></li>
@@ -13,6 +30,7 @@
 		<li><a href="Excluir_Pessoas">Excluir Pessoas</a></li>
 		<li><a href="Ver_Pessoas">Ver Pessoas</a></li>
 	</ul>
+	</div><!--container-->
 
 
 
