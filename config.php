ab1150aca1c33eaf91f34d49a0cacf4394602797
Vinicius da Silva <msn-dovinicius@hotmail.com>
2022-01-26 21:54:55 -0300

    Criei o mysql.php, e fiz o back-end do cadastrar

--- config.php
+++ config.php
@@ -10,5 +10,9 @@
 	spl_autoload_register($autoload);
 	
     define("INCLUDE_PATH", "http://localhost/Projeto_GerenciamentoPessoas/");
+    define("HOST", "localhost");
+    define("USER", "root");
+    define("PASSWORD",  "");
+    define("DATABASE", "db_pessoas");
 
 ?>
--- pages/Cadastrar_Pessoas.php
+++ pages/Cadastrar_Pessoas.php
@@ -5,7 +5,7 @@
 		
 		<div class="w100">
 		<div><label>Nome: </label></div>
-		<div><input type="text" name="Nome"/></div>
+		<div><input type="text" name="nome"/></div>
 		</div><!--w100-->
 
 		<div class="w100">
@@ -18,11 +18,27 @@
 		<div><input type="text" name="cpf"/></div>
 		</div><!--w100-->
 
-		<div class="w100">
-		<div><label>Senha: </label></div>
-		<div><input type="password" name="senha"/></div>
+		<!--<div class="w100">
+		<div><label>Foto: </label></div>
+		<div><input type="file" name="foto" /></div>
 		</div><!--w100-->
 
 		<input type="submit" name="acao" value="Cadastrar">
 	</form>
+
+		<?php
+
+			if(isset($_POST["acao"])){
+				$nome = $_POST["nome"];
+				$rg = $_POST["rg"];
+				$cpf = $_POST["cpf"];
+				$foto = "vinicius.png";
+
+				$sql = Mysql::conectar()->prepare("INSERT INTO cadastrar (id, nome,rg, cpf,foto) VALUES (null, ?, ?, ?, ?) ");
+
+				$sql->execute(array($nome,$rg,$cpf,$foto));
+
+			}
+
+		?>
 </div>
